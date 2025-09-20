a12eea7111eb78deb5cc6cfb29abaa307529dc4f
lalit9891 <lalit_9891@yahoo.com>
2025-09-20 17:03:38 +0530

    Module4 Assignment

--- Module_4.py
+++ Module_4.py
@@ -32,3 +32,7 @@ print("Sine: : ", sine)
 
 
 
+
+
+
+
--- testing.py
+++ testing.py
@@ -5,7 +5,8 @@ a = range(0,11,2)
 for i in a:
     print(i)
 
-'''
+
+
 
 #while loop
 
@@ -14,6 +15,15 @@ counter = 1
 while counter <=5:
     print("apple")
     counter = counter +1
+
+'''
+
+#return function
+
+def add1(a):
+    print (a+1)
+
+add1(5)
 
 
 
